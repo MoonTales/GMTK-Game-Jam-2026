31daa013aa777827dfafa19067fd8690aa0fa3e7
MoonTales <CohenLCalvert@gmail.com>
2026-07-24 20:30:31 -0600

    updates

--- Assets/Scripts/System/Initializer.cs
+++ Assets/Scripts/System/Initializer.cs
@@ -31,11 +31,11 @@ namespace System
         public static void Execute()
         {
             // Log initialization step for debugging and visibility.
-            //DebugUtils.Log("Loading persistent systems...");
+            DebugUtils.Log("Loading persistent systems...");
 
             // Load the "PERSISTANTOBJECTS" prefab from Resources,
             // instantiate it, and mark it to not be destroyed when changing scenes.
-            // UnityEngine.Object.DontDestroyOnLoad(UnityEngine.Object.Instantiate(UnityEngine.Resources.Load("PERSISTANTOBJECTS")));
+            UnityEngine.Object.DontDestroyOnLoad(UnityEngine.Object.Instantiate(UnityEngine.Resources.Load("PERSISTANTOBJECTS")));
         }
     }
 }
